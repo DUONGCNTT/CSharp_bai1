413f0d4220af16f487841b4db951b9644499542d
DUONGCNTT <nguyenduong2107207@gmail.com>
2023-09-04 14:06:53 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Bai1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Hello World " + i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
